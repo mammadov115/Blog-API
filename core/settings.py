436e9d3442206d2145424031bafda1b1bcd9fd07
mammadov115 <mehmanmehman182@gmail.com>
2023-08-22 17:10:53 +0400

    post model

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # local
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'posts.apps.PostsConfig'
 ]
 
 MIDDLEWARE = [
